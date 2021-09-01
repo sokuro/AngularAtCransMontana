28293110edefebd0232568cbce7df507ee1af041
Ugorcak, Karol Sokuro <karol.ugorcak@adesso.ch>
2021-09-01 10:12:40 +0200

    Added Category Classes incl. Interface

---
+++ src/models/category.ts
@@ -0,0 +1,56 @@
+export class Category {
+
+    public get description(): string {
+        return this._description;
+    }
+    public set description(value: string) {
+        this._description = value;
+    }
+    public get category(): Category | null {
+        return this._category;
+    }
+    public set category(value: Category | null) {
+        this._category = value;
+    }
+    public get categoryId(): number | null {
+        return this._categoryId;
+    }
+    public set categoryId(value: number | null) {
+        this._categoryId = value;
+    }
+    public get date(): Date {
+        return this._date;
+    }
+    public set date(value: Date) {
+        this._date = value;
+    }
+    public get title(): string {
+        return this._title;
+    }
+    public set title(value: string) {
+        this._title = value;
+    }
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+  
+    // declared variables do not have to be declared again as global variables 
+    constructor(
+        private _id: number = 0,
+        private _title: string = "",
+        private _date: Date = new Date(Date.now()),
+        private _categoryId: number | null = null,
+        private _category: Category | null = null,
+        private _description: string = "") 
+        {
+        }
+}
+
+export interface ICategory {
+    id: number;
+    title: string;
+    color: string;
+}
---
+++ src/models/to-do.ts
@@ -0,0 +1,71 @@
+import { Category } from "./category";
+
+export class ToDo {
+
+    get Id(): number {
+        return this.id;
+    }
+
+    set setId(value: number) {
+        this.id = value;
+    }
+
+    get Title(): string {
+        return this.title;
+    }
+
+    set Title(value: string) {
+        this.title = value;
+    }
+
+    get Date(): Date {
+        return this.date;
+    }
+
+    set Date(value: Date) {
+        this.date = value;
+    }
+
+    get Description(): string {
+        return this.description;
+    }
+
+    set Description(value: string) {
+        this.description = value;
+    }
+
+    get CategoryId(): number {
+        return this.categoryId;
+    }
+
+    set CategoryId(value: number) {
+        this.categoryId = value;
+    }
+
+    get Category(): Category {
+        return this.category;
+    }
+
+    set Category(value: Category) {
+        this.category = value;
+    }
+
+    constructor(
+        private id: number=0,
+        private title: string,
+        private date: Date=new Date(Date.now()),
+        private categoryId: number | null = null,
+        private category: Category | null = null,
+        private description: string="") 
+        {
+        }
+}
+
+export class IToDo {
+    id: number;
+    title: string;
+    date: Date;
+    description: string;
+    categoryId: number | null;
+    category: Category | null;
+}
